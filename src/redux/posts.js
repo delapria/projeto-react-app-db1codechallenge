9e7fd24a3621cf80f6114067924b6959dad6dd6d
delapria <delapria@gmail.com>
2019-08-31 08:42:36 -0300

    Refactory do redux actions

--- src/redux/posts.js
+++ src/redux/posts.js
@@ -1,26 +1,39 @@
-import { createAction } from 'redux-actions'
+import { createAction, handleActions } from 'redux-actions'
 
 //ACTIONS
 
 const POSTS_ADD_ACTION = 'POSTS/ADD';
+const POSTS_CLEAR_ACTION = 'POSTS/CLEAR';
+const POSTS_REMOVE_ACTION = 'POSTS/REMOVE';
 
 export const addPostAction = createAction(POSTS_ADD_ACTION, (description) => ({
   description
 }))
 
+export const clearPostAction = createAction(POSTS_CLEAR_ACTION)
+
+export const removePostAction = createAction (POSTS_REMOVE_ACTION)
+
 //REDUCERS
 
-const postsHandler = (state = [], action) => {
-  switch (action.type) {
-    case POSTS_ADD_ACTION: 
-      return [
-        ...state,
-        action.payload,
-      ];
-    default: 
-      return state;
-  }
-}
+const postsHandler = handleActions({
+  [POSTS_ADD_ACTION]: (state, action) => {
+    return [
+      ...state,
+      action.payload,
+    ];
+  },
+  [POSTS_CLEAR_ACTION]: () => {
+    return [];
+  },
+  [POSTS_REMOVE_ACTION]: (state, action) => {
+    //const newState = [...state];
+    //newState.splice(action.payload, 1)
+    //return newState;
+
+    return state.filter((post, index) => index !== action.payload)
+  },
+}, [])
 
 export const reducers = {
   posts: postsHandler,
